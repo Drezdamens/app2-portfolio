dc3bf06b730f041c6aab7cd690bd1e501fb08187
Lil127moone <lil127moone@gmail.com>
2024-11-04 20:42:19 +0200

    Send email

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 
 st.header("Contact me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Enter your email")
+    message = st.text_area("Enter you issue")
+    button = st.form_submit_button("Submit")
+    print(button)
+    if button:
+        print("Pressed")
